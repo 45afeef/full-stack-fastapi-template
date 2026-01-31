39226b0029daa4a1391ba32519851b16613f16a9
Afeef K K <45afeef@gmail.com>
2026-01-31 13:05:40 +0530

    fix: update SECRET_KEY configuration to use environment variable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,4 @@
-import secrets
+# import secrets
 import warnings
 from typing import Annotated, Any, Literal
 
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
         extra="ignore",
     )
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
@@ -48,6 +48,7 @@ class Settings(BaseSettings):
             self.FRONTEND_HOST
         ]
 
+    SECRET_KEY: str
     PROJECT_NAME: str
     SENTRY_DSN: HttpUrl | None = None
     POSTGRES_SERVER: str
